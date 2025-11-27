83d96d63ebb591ca52f57928e6698f6f90f53add
Gustavo Orpinelli de Marco <112019190+GustavoDMarco@users.noreply.github.com>
2025-11-26 21:55:30 -0300

    Add files via upload

---
+++ connection_mongo.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import time
+from pymongo import MongoClient
+
+# ---------------------------
+# CONEXÃO COM O MONGODB ATLAS
+# ---------------------------
+
+MONGO_URI = "mongodb+srv://gustavomarco_db_user:gustavo2002@lab-bd.50wjwhy.mongodb.net/?appName=lab-bd"
+
+client = MongoClient(MONGO_URI)
+# Banco para currículos
+db_curriculos = client["curriculos"]
+curriculos = db_curriculos["curriculos"]
+
+# Banco para vagas
+db_vagas = client["vagas"]
+vagas = db_vagas["vagas"]
